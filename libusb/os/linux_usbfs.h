c57c40ff9e44499c1743a60eccb2558616635fc0
Radu Vele <raduvele@google.com>
2024-10-18 16:42:34 +0200

    Enable linux netlink event monitoring for Android OS platform services
    Introducing the ANDROID_OS macro that is supposed to be set by clients
    of libusb that are running on Android at the OS level.
    
    If Android_OS is set (and HAVE_LIBUDEV is not) then linux netlink event
    monitoring (and consequently hotplug functionality) is enabled. This
    works for Android services or simple binaries running at the OS level as
    they have the necessary permissions, in contrast to usual Android apps
    (see commit 2f3bc98).
    
    Note: the __ANDROID__ macro is set by GCC for all targets running on
    Android (both OS-level services and apps).

--- libusb/os/linux_usbfs.h
+++ libusb/os/linux_usbfs.h
@@ -174,7 +174,7 @@ static inline int linux_start_event_monitor(void)
 {
 #if defined(HAVE_LIBUDEV)
 	return linux_udev_start_event_monitor();
-#elif !defined(__ANDROID__)
+#elif !defined(__ANDROID__) || defined(ANDROID_OS)
 	return linux_netlink_start_event_monitor();
 #else
 	return LIBUSB_SUCCESS;
@@ -185,7 +185,7 @@ static inline void linux_stop_event_monitor(void)
 {
 #if defined(HAVE_LIBUDEV)
 	linux_udev_stop_event_monitor();
-#elif !defined(__ANDROID__)
+#elif !defined(__ANDROID__) || defined(ANDROID_OS)
 	linux_netlink_stop_event_monitor();
 #endif
 }
@@ -194,7 +194,7 @@ static inline void linux_hotplug_poll(void)
 {
 #if defined(HAVE_LIBUDEV)
 	linux_udev_hotplug_poll();
-#elif !defined(__ANDROID__)
+#elif !defined(__ANDROID__) || defined(ANDROID_OS)
 	linux_netlink_hotplug_poll();
 #endif
 }
